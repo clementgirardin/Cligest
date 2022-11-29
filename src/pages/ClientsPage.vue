64bc2e23dd8ab1f9a8e7e06c43da8edad0c1fd7e
giracle <clement.girardin@divtec.ch>
2022-11-29 12:31:30 +0100

    Initial commit

---
+++ src/pages/ClientsPage.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>coucou</h1>
+</template>
+
+<script>
+export default {
+  name: 'ClientsPage.vue',
+  components: {}
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page padding>
+    <h3>CliGest</h3>
+    <p class="text-subtitle1">L'application pour gérer vos clients !</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
